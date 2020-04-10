7d4eb3ec22633018d6dc316692b8c4e70d44bd4e
4wa4k3 <andres@ajvelasquez.com>
2020-04-10 14:08:55 -0400

    Added language switcher to Default layouts.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <div>
+      <nuxt-link
+        v-for="locale in availableLocales"
+        :key="locale.code"
+        :to="switchLocalePath(locale.code)"
+      >
+        {{ locale.name }}
+      </nuxt-link>
+    </div>
     <nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    }
+  }
+}
+</script>
